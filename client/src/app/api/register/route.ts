27356f5b4d361ee0aaee2f457f01f39fe1f9d11d
igachev <ivophoenix1135@gmail.com>
2024-06-12 01:28:38 +0300

    created endpoint for register user

---
+++ client/src/app/api/register/route.ts
@@ -0,0 +1,24 @@
+import User from "@/models/User"
+import { initDB } from "@/utils/database"
+
+export const POST = async(req:any,res: any) => {
+    const {email,password,username,profileImage} = await req.json()
+    try {
+        await initDB()
+
+        const user = await User.findOne({email:email});
+        if(user) {
+            throw new Error("User already exists!")
+        }
+
+        const newUser = new User({email,password,username,profileImage})
+        await newUser.save()
+
+        return new Response(JSON.stringify(newUser), {status: 201})
+    } catch (err:any) {
+        return new Response(JSON.stringify(err.message),{status: 500})
+    }
+}
+
+
+
