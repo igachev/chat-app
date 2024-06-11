f5c9069c1ae100fd6011dc2bb59ea6030a2d8828
igachev <ivophoenix1135@gmail.com>
2024-06-12 00:42:09 +0300

    initial User model

---
+++ client/src/models/User.ts
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true,'email is required']
+    },
+    password: {
+        type: String,
+        required: [true,'password is required']
+    },
+    username: {
+        type: String,
+        required: [true,'username is required']
+    },
+    profileImage: {
+        type: String,
+        default: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"
+    }
+})
+
+userSchema.pre('save',async function(this: typeof userSchema) {
+    const hashedPassword = await bcrypt.hash(this.password,10)
+    this.password = hashedPassword
+})
+
+userSchema.methods.validatePassword = async function(addedPassword: string) {
+    const result = await bcrypt.compare(addedPassword,this.password)
+    return result
+}
+
+const User = mongoose.model("User",userSchema)
+
+module.exports = User
