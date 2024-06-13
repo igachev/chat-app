4915e5fd99645c36af066c0835b57d7136679e01
igachev <ivophoenix1135@gmail.com>
2024-06-13 23:32:10 +0300

    added 2 if statements in the middleware
    1st if is for protected routes
    2nd is to prevent going to /login /register if user is already logged in

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,6 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/','/login(.*)', '/register(.*)']);
+
+
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
+
+  if(auth().userId && request.nextUrl.pathname === '/login' || request.nextUrl.pathname === '/register') {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
