cc29344f0f80967d405de09fb9c96417b85a906d
igachev <ivophoenix1135@gmail.com>
2024-06-22 23:33:38 +0300

    edited the response of endpoint /register and added interfaces

--- client/src/app/api/register/route.ts
+++ client/src/app/api/register/route.ts
@@ -1,8 +1,20 @@
 import User from "@/models/User"
 import { initDB } from "@/utils/database"
 
+interface UserData {
+    userId: string;
+    username: string;
+}
+
+interface RequestBody {
+    email: string;
+    password: string;
+    username: string;
+    profileImage?: string;
+}
+
 export const POST = async(req:any,res: any) => {
-    const {email,password,username,profileImage} = await req.json()
+    const {email,password,username,profileImage}: RequestBody = await req.json()
     try {
         await initDB()
 
@@ -13,8 +25,8 @@ export const POST = async(req:any,res: any) => {
 
         const newUser = new User({email,password,username,profileImage})
         await newUser.save()
-
-        return new Response(JSON.stringify(newUser), {status: 201})
+        const userData: UserData = {userId: newUser._id, username: newUser.username}
+        return new Response(JSON.stringify(userData), {status: 201})
     } catch (err:any) {
         return new Response(JSON.stringify(err.message),{status: 500})
     }
