a3f596b3a7faf76db002a9332084df4d57db6c9a
igachev <ivophoenix1135@gmail.com>
2024-06-25 12:22:28 +0300

    edited Header component
    added 2 useEffect hooks:
    1) to listen for the custom dispatch event  and to check if there is information inside the local storage and if so to update the userData state
    2) if the user is not logged in clear the state of userData and clear the local storage

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,39 @@
 "use client"
+import { UserData } from '@/app/chat/page'
 import { UserButton, useAuth } from '@clerk/nextjs'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
 
   let { isSignedIn } = useAuth();
+  const [userData,setUserData] = useState<UserData | null>(null)
+
+  useEffect(() => {
+    const updateUserData = () => {
+      const userDataString = localStorage.getItem("userData");
+      if (userDataString) {
+        const data = JSON.parse(userDataString);
+        setUserData(data);
+      }
+    };
+
+    updateUserData(); // Initial check
+
+    // Listen for custom event
+    window.addEventListener('userDataChanged', updateUserData);
+
+    return () => {
+      window.removeEventListener('userDataChanged', updateUserData);
+    };
+  }, []);
+
+  useEffect(() => {
+    if(!isSignedIn) {
+      setUserData(null)
+      localStorage.removeItem("userData")
+    }
+  },[isSignedIn])
   
   return (
     <>
@@ -22,6 +50,10 @@ const Header = () => {
               <div >
                 <UserButton afterSignOutUrl="/" />
               </div>
+            )}
+
+            {userData && (
+              <h3>Welcome, {userData?.username}!</h3>
             )}
           
         </header>
