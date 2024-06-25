0e904697e60a51fbe4c7dd02becc498848043593
igachev <ivophoenix1135@gmail.com>
2024-06-25 12:23:44 +0300

    edited / route
    added Link to /chat route.If the user is not logged in it will redirect him to /login page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="bg-slate-500 min-h-[900px] text-white text-center pt-4">
       <h1 className="text-7xl mb-3">Welcome!</h1>
       <h3 className="text-2xl font-light">If you want to chat with other users on different topics please register and login</h3>
+      <div>
+        <Link href="/chat">
+        <Button variant="default">Go To Chat Page</Button>
+        </Link>
+      </div>
     </div>
   );
 }
